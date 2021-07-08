fb314f742aafeaeeed07cccc5806656689045af7
marcosarto <marcosartorelli24@gmail.com>
2021-07-08 10:32:57 +0200

    nuovi train

--- fill.m
+++ fill.m
@@ -1,7 +1,7 @@
 function [blocchi,k] = fill(N,M,blocchi)
     k=1;
-    for i=24:6:N-24
-        for j=24:6:M-24
+    for i=24:16:N-24
+        for j=24:16:M-24
             mat=generaMat(i,j);
             disp(mat)
             blocchi{k}=mat;
